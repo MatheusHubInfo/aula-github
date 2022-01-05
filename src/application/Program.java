e9978a8779398fa49c9cd8b6247974f64a9767af
Matheus Alves <mm5426875@gmail.com>
2022-01-04 21:15:08 -0300

    Horario BR

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,24 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Program {
 
-	public static void main(String[] args) {
-	
-		System.out.println("Bom dia");
-		System.out.println("Boa Tarde");
-		System.out.println("Boa noite");
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sfd1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sfd2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		Date y1 = sfd1.parse("25/06/2018");
+		Date y2 = sfd2.parse("25/06/2018 15:42:07");
+
+		System.out.println("y1 : " + sfd1.format(y1));
+		System.out.println("y2 : " + sfd2.format(y2));
+
+		System.out.println(y1);
+		System.out.println(y2);
 
 	}
-
 }
